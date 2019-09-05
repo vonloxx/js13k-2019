a5f878f61240855a84124cc4e547402047e957c6
Marco Fernandes <marco@front.no>
2019-09-05 17:39:31 +0100

    Initial hero (player) component.

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,22 +1,54 @@
 import compose from "../lib/compose";
 import Component from "./component";
+import Controller from "./controller";
+import Spritesheet from "./spritesheet";
 
-export default (wrapped) => {
-  const { state, update, render, setState} = wrapped;
+export default (original) => {
+  const { gameState, update, render } = original;
+  const { assets } = gameState;
   let timer = 0;
 
-  return compose(Component)({
+  return compose(Component, Spritesheet, Controller)({
     state: {
       x: 0,
       y: 0,
+      keys: {
+        left: 37,
+        right: 39,
+        jump: 32,
+      },
     },
-    ...wrapped,
+    width: 16,
+    height: 16,
+    image: assets.getAsset('spritesheet'),
+    animations: {
+      idle: {
+        frames: [1],
+      },
+      walk: {
+        frames: [1, 2, 3, 4, 5],
+        speed: 5,
+        direction: 'alternate',
+      },
+      jump: {
+        frames: [1],
+      },
+      fall: {
+        frames: [6],
+      },
+      talk: {
+        frames: [1, 6],
+        speed: 15,
+        direction: 'alternate',
+      },
+    },
+    ...original,
     update(props) {
       update && update(props);
       const { setState } = props;
 
       timer++;
-      setState({x: ~~(100 + Math.sin(timer/100) * 100)});
+      setState({x: ~~(100 + Math.sin(timer/100) * 300)});
     },
 
     render(props) {
